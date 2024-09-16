1268a50e07335311b5436da5e02f20856fc5388e
Bankole Azeez Babatunde <bankoleazeezb98@gmail.com>
2024-09-16 20:23:23 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./App.css";
+import Home from "./Components/Home";
+import AllPost from "./Components/AllPost";
+import PostDetail from "./Components/PostDetail";
+import Login from "./Components/Login";
+import Editpost from "./Components/Editpost";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Contact from "./Components/Contact";
+import Scrow from "./Components/Scrow";
+import Password from './Components/Password';
+import Register from './Components/Register';
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Scrow />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/AllPost" element={<AllPost />} />
+          <Route path="/PostDetail/:id" element={<PostDetail />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Editpost/:id" element={<Editpost />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Password" element={<Password/>} />
+          <Route path="/Register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
